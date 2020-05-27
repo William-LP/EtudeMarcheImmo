2ce6dee91a2070db80a7ac9193c6c40fba3c5b5f
William-LP <william.lepommelet@gmail.com>
2020-05-28 01:18:52 +0200

    parse web page

--- maison-a-renover.py
+++ maison-a-renover.py
@@ -1,6 +1,16 @@
+# Import libraries
+import requests
+from bs4 import BeautifulSoup
 import urllib.request
 
 for i in range(0, 328, 8):
     url = "http://www.maison-a-renover.fr/immobilier/toutes-annonces?start=" + str(i)
-    if (urllib.request.urlopen("http://www.stackoverflow.com").getcode()) == 200:
-        print(url + " : OK")
+    if (urllib.request.urlopen(url).getcode()) == 200:
+        # print(url + " : OK")
+        page = requests.get(url)
+        soup = BeautifulSoup(page.text, 'html.parser')
+
+        spans = soup.find_all("div", class_="mid94")
+        for span in spans:
+            cp = span.find("span", {"class": "lkdv"}).text
+            print(cp)
