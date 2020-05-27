6c449731fedb6f7b588fe74de8be23cd3961e640
William-LP <william.lepommelet@gmail.com>
2020-05-28 01:37:54 +0200

    sum data within a hash table

--- maison-a-renover.py
+++ maison-a-renover.py
@@ -3,7 +3,9 @@ import requests
 from bs4 import BeautifulSoup
 import urllib.request
 
-for i in range(0, 328, 8):
+code_postal = {}
+
+for i in range(0, 328, 8):  # for i in range(0, 8, 8):
     url = "http://www.maison-a-renover.fr/immobilier/toutes-annonces?start=" + str(i)
     if (urllib.request.urlopen(url).getcode()) == 200:
         # print(url + " : OK")
@@ -13,4 +15,10 @@ for i in range(0, 328, 8):
         spans = soup.find_all("div", class_="mid94")
         for span in spans:
             cp = span.find("span", {"class": "lkdv"}).text
-            print(cp)
+            if cp in code_postal:
+                code_postal[cp] += 1
+            else:
+                code_postal[cp] = 1
+
+for k, v in code_postal.items():
+    print(k, ':', v)
