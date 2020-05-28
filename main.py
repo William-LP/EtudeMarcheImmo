7a66db4b8d9887f148d3c93f58a93dea92982caa
William-LP <william.lepommelet@gmail.com>
2020-05-28 05:10:39 +0200

    add a progression counter

--- main.py
+++ main.py
@@ -5,15 +5,25 @@ import urllib.request
 import re
 import datetime
 
+# http://www.maison-a-renover.fr/immobilier/toutes-annonces?start=1
+# https://www.proprietes-privees.com/achat/renovation?page=1&controller=Application%5CController%5CAchat&action=index&type=renovation
+# https://www.superimmo.com/achat/maison/ile-de-france,rhone-alpes,provence-alpes-cote-d-azur,nord-pas-de-calais,pays-de-la-loire,bretagne,midi-pyrenees,centre,lorraine,languedoc-roussillon,picardie,haute-normandie,alsace,poitou-charentes,bourgogne,basse-normandie,champagne-ardenne,auvergne,franche-comte,limousin,corse/a-renover/p/1
+
+maison_a_renover_max = 320
+maison_a_renover_pas = 8
+proprietes_privees_max = 22
+proprietes_privees_pas = 1
+superimmo_max = 1384
+superimmo_pas = 1
 
 now = datetime.datetime.now()
-print("Starting time :")
-print(now.strftime("%Y-%m-%d %H:%M:%S"))
+print("Starting time : " + now.strftime("%Y-%m-%d %H:%M:%S"))
 
 code_postal = {}
-
-print("Source : maison-a-renover.fr ...")
-for i in range(0, 328, 8):  # for i in range(0, 8, 8):
+print("\n")
+print("SCRAPPING : ")
+for i in range(0, maison_a_renover_max + maison_a_renover_pas, maison_a_renover_pas):
+    print('maison-a-renover.fr' + ' [%d%%]\r' % (i * 100 / maison_a_renover_max), end="")
     url = "http://www.maison-a-renover.fr/immobilier/toutes-annonces?start=" + str(i)
     if (urllib.request.urlopen(url).getcode()) == 200:
         # print(url + " : OK")
@@ -27,8 +37,9 @@ for i in range(0, 328, 8):  # for i in range(0, 8, 8):
                 code_postal[cp] += 1
             else:
                 code_postal[cp] = 1
-print("Source : proprietes-privees.com ...")
-for i in range(1, 23, 1):  # for i in range(0, 8, 8):
+
+for i in range(1, proprietes_privees_max, proprietes_privees_pas):  # for i in range(0, 8, 8):
+    print('proprietes-privees.com' + ' [%d%%]\r' % (i * 100 / proprietes_privees_max), end="")
     url = "https://www.proprietes-privees.com/achat/renovation?page=" + str(i) + "&controller=Application%5CController%5CAchat&action=index&type=renovation"
     if (urllib.request.urlopen(url).getcode()) == 200:
         # print(url + " : OK")
@@ -44,8 +55,8 @@ for i in range(1, 23, 1):  # for i in range(0, 8, 8):
                 code_postal[cp] = 1
 
 
-print("Source : superimmo.com ...")
-for i in range(1, 1385, 1):
+for i in range(1, superimmo_max, superimmo_pas):
+    print('superimmo.com - [%d%%]\r' % (i * 100 / superimmo_max), end="")
     url = "https://www.superimmo.com/achat/maison/ile-de-france,rhone-alpes,provence-alpes-cote-d-azur,nord-pas-de-calais,pays-de-la-loire,bretagne,midi-pyrenees,centre,lorraine,languedoc-roussillon,picardie,haute-normandie,alsace,poitou-charentes,bourgogne,basse-normandie,champagne-ardenne,auvergne,franche-comte,limousin,corse/a-renover/p/" + \
         str(i)
     if (urllib.request.urlopen(url).getcode()) == 200:
@@ -69,5 +80,4 @@ for k, v in code_postal.items():
 
 
 now = datetime.datetime.now()
-print("Ending time :")
-print(now.strftime("%Y-%m-%d %H:%M:%S"))
+print("Ending time : " + now.strftime("%Y-%m-%d %H:%M:%S"))
