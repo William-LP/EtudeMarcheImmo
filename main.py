58b019c1929b7f0022ea948a27439038024614b8
William-LP <william.lepommelet@gmail.com>
2020-05-28 03:02:11 +0200

    merge both files, ugly but busy

---
+++ main.py
@@ -0,0 +1,40 @@
+# Import libraries
+import requests
+from bs4 import BeautifulSoup
+import urllib.request
+import re
+
+code_postal = {}
+
+for i in range(0, 328, 8):  # for i in range(0, 8, 8):
+    url = "http://www.maison-a-renover.fr/immobilier/toutes-annonces?start=" + str(i)
+    if (urllib.request.urlopen(url).getcode()) == 200:
+        # print(url + " : OK")
+        page = requests.get(url)
+        soup = BeautifulSoup(page.text, 'html.parser')
+
+        spans = soup.find_all("div", class_="mid94")
+        for span in spans:
+            cp = span.find("span", {"class": "lkdv"}).text
+            if cp in code_postal:
+                code_postal[cp] += 1
+            else:
+                code_postal[cp] = 1
+
+for i in range(1, 23, 1):  # for i in range(0, 8, 8):
+    url = "https://www.proprietes-privees.com/achat/renovation?page=" + str(i) + "&controller=Application%5CController%5CAchat&action=index&type=renovation"
+    if (urllib.request.urlopen(url).getcode()) == 200:
+        # print(url + " : OK")
+        page = requests.get(url)
+        soup = BeautifulSoup(page.text, 'html.parser')
+
+        h4s = soup.find_all("h4", class_="offer-city-name")
+        for h4 in h4s:
+            cp = re.sub("[^0-9]", "", h4.text)
+            if cp in code_postal:
+                code_postal[cp] += 1
+            else:
+                code_postal[cp] = 1
+
+for k, v in code_postal.items():
+    print(k, ':', v)
